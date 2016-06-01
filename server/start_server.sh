898d0121ca434c8569c2bcff67c467a062bcf86f
Anthony Ogundipe <diltony@yahoo.com>
2016-06-01 21:16:37 +0100

    Updated

--- server/start_server.sh
+++ server/start_server.sh
@@ -3,4 +3,13 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCR="$DIR/server.php"
 
-nohup php $SCR > error_log &
+haySack=ps auxwww|grep -i 'server.php'
+
+appName=server.php
+
+if [ "${haySack/$appName}" = "$haySack" ] ; then
+    echo "${appName} will be restarted now"
+	nohup php $SCR > error_log &
+else
+  echo "${appName} is running properly"
+fi
